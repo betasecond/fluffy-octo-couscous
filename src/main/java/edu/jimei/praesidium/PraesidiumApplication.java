df05e98f9c21ad12d199ac11fbf78bcb6226d244
betasecond <nbdhc9039@gmail.com>
2025-07-13 12:54:28 +0800

    docs: add javadoc to main application class

--- src/main/java/edu/jimei/praesidium/PraesidiumApplication.java
+++ src/main/java/edu/jimei/praesidium/PraesidiumApplication.java
@@ -3,9 +3,16 @@ package edu.jimei.praesidium;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main entry point for the Praesidium Spring Boot application.
+ */
 @SpringBootApplication
 public class PraesidiumApplication {
 
+    /**
+     * The main method which serves as the entry point for the application.
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         SpringApplication.run(PraesidiumApplication.class, args);
     }
