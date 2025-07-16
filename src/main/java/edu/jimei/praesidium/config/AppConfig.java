eca2c36aa9feac03dd8ede6567ea805a72ae60b9
何昕昊 <xhhdaxx@gmail.com>
2025-07-16 21:11:23 +0800

    更新了时间导致的报错

--- src/main/java/edu/jimei/praesidium/config/AppConfig.java
+++ src/main/java/edu/jimei/praesidium/config/AppConfig.java
@@ -7,13 +7,18 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class AppConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
     }
 
     @Bean
@@ -29,3 +34,4 @@ public class AppConfig {
         return modelMapper;
     }
 }
+
